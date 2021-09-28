3838b57beb18a811934978557ee4c9fe0e9b663a
pastorcoder <zuma4good@gmail.com>
2021-09-28 20:25:04 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+function Toggle() {
+  const [isToggleOn, setIsToggleOn] = useState(true);
+
+  const handleClick = () => {
+    setIsToggleOn(!isToggleOn);
+  };
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button className="toggle-btn" onClick={handleClick}>
+        {isToggleOn ? "ON"  : "OFF"}
+      </button>
+      
+    </>
   );
 }
 
-export default App;
+export default Toggle;
